69d39b18b77f67c5ac478602bdb53401bedef06c
Joshua Baxter <joshbax@microsoft.com>
2017-08-15 16:36:17 -0700

    added Enemy class creating list of Enemies from site data Enemy form output capture fixed rectangle, save to file, ocr on file, output to textbox tested on running game; mostly successful

---
+++ ff_ocr/Enemy.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ff_ocr {
+    class Enemy {
+        public List<string> MatchStrings = new List<string>();
+
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string Level { get; set; }
+        public string HP { get; set; }
+        public string Attack { get; set; }
+        public string HitPercentage { get; set; }
+        public string MagicAttack { get; set; }
+        public string Speed { get; set; }
+        public string Defense { get; set; }
+        public string Evasion { get; set; }
+        public string MagicDefense { get; set; }
+        public string MagicEvasion { get; set; }
+        public string GP { get; set; }
+        public string Experience { get; set; }
+        public string Weaknesses { get; set; }
+        public string Absorbs { get; set; }
+        public string Resists { get; set; }
+        public string Steal { get; set; }
+        public string Drop { get; set; }
+
+        public Enemy(XElement e) {
+            XElement eFirstRow = e.Element("tr");
+            Name = eFirstRow.Element("td").Element("b").Value;
+
+            //
+
+            XElement eSecondRow = eFirstRow.ElementsAfterSelf().First();
+            ImageURL = "http://www.finalfantasykingdom.net/" + eSecondRow.Element("td").Element("img").Attribute("src").Value;
+
+            //
+
+            XElement eThirdRow = eSecondRow.ElementsAfterSelf().First();
+
+            XElement eLevel = eThirdRow.Element("td");
+            Level = eLevel.Value;
+
+            XElement eHP = eLevel.ElementsAfterSelf().First();
+            HP = eHP.Value;
+
+            XElement eAttack = eHP.ElementsAfterSelf().First();
+            Attack = eAttack.Value;
+
+            XElement eHitPercentage = eAttack.ElementsAfterSelf().First();
+            HitPercentage = eHitPercentage.Value;
+
+            XElement eMagicAttack = eHitPercentage.ElementsAfterSelf().First();
+            MagicAttack = eMagicAttack.Value;
+
+            XElement eSpeed = eMagicAttack.ElementsAfterSelf().First();
+            Speed = eSpeed.Value;
+
+            XElement eDefense = eSpeed.ElementsAfterSelf().First();
+            Defense = eDefense.Value;
+
+            XElement eEvasion = eDefense.ElementsAfterSelf().First();
+            Evasion = eEvasion.Value;
+
+            XElement eMagicDefense = eEvasion.ElementsAfterSelf().First();
+            MagicDefense = eMagicDefense.Value;
+
+            XElement eMagicEvasion = eMagicDefense.ElementsAfterSelf().First();
+            MagicEvasion = eMagicEvasion.Value;
+
+            //
+
+            // skip fourth row
+            XElement eFourthRow = eThirdRow.ElementsAfterSelf().First();
+
+            //
+
+            XElement eFifthRow = eFourthRow.ElementsAfterSelf().First();
+
+            XElement eGP = eFifthRow.Element("td");
+            GP = eGP.Value;
+
+            XElement eExperience = eGP.ElementsAfterSelf().First();
+            Experience = eExperience.Value;
+
+            XElement eWeaknesses = eExperience.ElementsAfterSelf().First();
+            StringBuilder sb = new StringBuilder();
+            foreach (XElement eImg in eWeaknesses.Elements("img")) {
+                sb.Append(eImg.Attribute("src").Value.Replace(".png", "").Replace("4/", "") + ", ");
+            }
+            if (sb.Length > 0) {
+                sb.Remove(sb.Length - 2, 2);
+                Weaknesses = sb.ToString();
+            }
+            else {
+                Weaknesses = "(none)";
+            }
+            
+
+            XElement eAbsorbs = eWeaknesses.ElementsAfterSelf().First();
+            sb = new StringBuilder();
+            foreach (XElement eImg in eAbsorbs.Elements("img")) {
+                sb.Append(eImg.Attribute("src").Value.Replace(".png", "").Replace("4/", "") + ", ");
+            }
+            if (sb.Length > 0) {
+                sb.Remove(sb.Length - 2, 2);
+                Absorbs = sb.ToString();
+            }
+            else {
+                Absorbs = "(none)";
+            }
+
+            XElement eResists = eAbsorbs.ElementsAfterSelf().First();
+            sb = new StringBuilder();
+            foreach (XElement eImg in eResists.Elements("img")) {
+                sb.Append(eImg.Attribute("src").Value.Replace(".png", "").Replace("4/", "") + ", ");
+            }
+            if (sb.Length > 0) {
+                sb.Remove(sb.Length - 2, 2);
+                Resists = sb.ToString();
+            }
+            else {
+                Resists = "(none)";
+            }
+        }
+
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
--- ff_ocr/Form1.cs
+++ ff_ocr/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using Windows.Graphics.Imaging;
 using Windows.Media.Ocr;
 using Windows.Storage;
@@ -17,6 +19,14 @@ namespace ff_ocr {
         OcrEngine ocr;
         SoftwareBitmap bitmap;
         Bitmap bmp;
+        bool bReady = true;
+        List<Enemy> Enemies = new List<Enemy>();
+
+        int width = 389;
+        int height = 292;
+        int x = 433;
+        int y = 719;
+        char[] filters = { ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
         public Form1() {
             InitializeComponent();
@@ -24,21 +34,27 @@ namespace ff_ocr {
 
         private async void Form1_Load(object sender, EventArgs e) {
             ocr = OcrEngine.TryCreateFromUserProfileLanguages();
-            bmp = new Bitmap(256, 256);
+            bmp = new Bitmap(width, height);
+
+            XDocument doc = XDocument.Load(@"c:\users\joshbax\desktop\tables.xml");
+            foreach (XElement eTable in doc.Root.Elements("table")) {
+                Enemy enemy = new Enemy(eTable);
+                Enemies.Add(enemy);
+                listBox1.Items.Add(enemy);
+            }
         }
 
-        private async void button1_Click(object sender, EventArgs e) {
+        private async Task Recognize() {
+            string path = @"c:\users\joshbax\desktop\temp.bmp";
+            Stopwatch s = Stopwatch.StartNew();
+            richTextBox1.Clear();
+
             using (Graphics g = Graphics.FromImage(bmp)) {
-                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X + x, Screen.PrimaryScreen.Bounds.Y + y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
             }
 
-            pictureBox1.Image = bmp;
-            string path = @"c:\users\baxte\desktop\temp.bmp";
             bmp.Save(path);
-            await Recognize(path);
-        }
 
-        private async Task Recognize(string path) {
             StorageFile input = await StorageFile.GetFileFromPathAsync(path);
             using (IRandomAccessStream stream = await input.OpenAsync(FileAccessMode.Read)) {
                 BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
@@ -47,8 +63,43 @@ namespace ff_ocr {
 
             OcrResult result = await ocr.RecognizeAsync(bitmap);
             foreach (OcrLine line in result.Lines) {
-                richTextBox1.AppendText(line.Text + "\r\n");
+                richTextBox1.AppendText(String.Join("", line.Text.Split(filters, StringSplitOptions.RemoveEmptyEntries)) + "\r\n");
             }
+
+            s.Stop();
+            richTextBox1.AppendText("Time: " + s.ElapsedMilliseconds.ToString() + " ms\r\n");
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e) {
+            if (!bReady) { return; }
+
+            bReady = false;
+            await Recognize();
+            bReady = true;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            if (listBox1.SelectedIndex == -1) { return; }
+
+            Enemy enemy = (Enemy)listBox1.SelectedItem;
+            txtName.Text = enemy.Name;
+            txtLevel.Text = enemy.Level;
+            txtHP.Text = enemy.HP;
+            txtAttack.Text = enemy.Attack;
+            txtHitPercentage.Text = enemy.HitPercentage;
+            txtMagicAttack.Text = enemy.MagicAttack;
+            txtSpeed.Text = enemy.Speed;
+            txtDefense.Text = enemy.Defense;
+            txtEvasion.Text = enemy.Evasion;
+            txtMagicDefense.Text = enemy.MagicDefense;
+            txtMagicEvasion.Text = enemy.MagicEvasion;
+            txtGP.Text = enemy.GP;
+            txtExperience.Text = enemy.Experience;
+            txtWeaknesses.Text = enemy.Weaknesses;
+            txtAbsorbs.Text = enemy.Absorbs;
+            txtResists.Text = enemy.Resists;
+
+            pictureBox1.Load(enemy.ImageURL);
         }
     }
 }
